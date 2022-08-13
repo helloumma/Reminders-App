21d8d1545f5ea8042f7e3c6ad547694cf9c910d3
umma <ummagohil@gmail.com>
2022-08-13 22:45:25 +0100

    Set up

---
+++ src/components/calendar.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { enGB } from "date-fns/locale";
+import { DatePicker } from "react-nice-dates";
+import "react-nice-dates/build/style.css";
+
+/*
+interface Props = {
+    
+} */
+
+const Calendar = () => {
+  const [date, setDate] = useState<any>(new Date());
+
+  const timeInputProps = {
+    date,
+    format: "HH:mm",
+    locale: enGB,
+    onDateChange: setDate,
+  };
+
+  return (
+    <div style={{ display: "flex" }}>
+      <DatePicker
+        date={date}
+        onDateChange={setDate}
+        locale={enGB}
+        format="dd/MM/yyyy"
+      >
+        {({ inputProps, focused }) => (
+          <input
+            className={"input" + (focused ? " -focused" : "")}
+            style={{ width: 150 }}
+            {...inputProps}
+          />
+        )}
+      </DatePicker>
+      <input
+        className="input"
+        style={{ marginLeft: 16, width: 80 }}
+        {...timeInputProps}
+      />
+    </div>
+  );
+};
+export default Calendar;
---
+++ src/components/form.tsx
@@ -0,0 +1,15 @@
+import Calendar from "./calendar";
+
+const Form = () => (
+  <form>
+    <label>time</label>
+    <input />
+    <label>date</label>
+    <Calendar />
+    <label>reminder</label>
+    <input />
+    <button>submit</button>
+  </form>
+);
+
+export default Form;
