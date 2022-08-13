5dac8cc5d967d5dc17b25d7c8a181a880507acd8
ummagohil <756d6d61@gmail.com>
2022-08-13 22:56:45 +0100

    formatitng

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -3,6 +3,8 @@ import { enGB } from "date-fns/locale";
 import { DatePicker } from "react-nice-dates";
 import "react-nice-dates/build/style.css";
 
+// TO DO: switch to TypeScript
+
 /*
 interface Props = {
     test 2
@@ -12,22 +14,20 @@ const Calendar = () => {
   const [date, setDate] = useState<any>(new Date());
 
   return (
-    <div style={{ display: "flex" }}>
-      <DatePicker
-        date={date}
-        onDateChange={setDate}
-        locale={enGB}
-        format="dd/MM/yyyy"
-      >
-        {({ inputProps, focused }) => (
-          <input
-            className={"input" + (focused ? " -focused" : "")}
-            style={{ width: 150 }}
-            {...inputProps}
-          />
-        )}
-      </DatePicker>
-    </div>
+    <DatePicker
+      date={date}
+      onDateChange={setDate}
+      locale={enGB}
+      format="dd/MM/yyyy"
+    >
+      {({ inputProps, focused }) => (
+        <input
+          className={"input" + (focused ? " -focused" : "")}
+          style={{ width: 150 }}
+          {...inputProps}
+        />
+      )}
+    </DatePicker>
   );
 };
 export default Calendar;
