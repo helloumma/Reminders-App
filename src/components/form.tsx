92e2dd5ef536c6424e76d125767fefff888d0347
umma <756d6d61@gmail.com>
2022-08-21 17:30:37 +0100

    form styles

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -7,33 +7,39 @@ interface Props {
 const Form = ({ handleChange, handleSubmit, val }: Props) => {
   return (
     <form onSubmit={(e) => handleSubmit(e)} className="form">
-      <label>time</label>
-      <input
-        className=""
-        id=""
-        type="text"
-        onChange={(e) => handleChange(e)}
-        name="time"
-        value={val.time}
-      />
-      <label>date</label>
-      <input
-        className=""
-        id=""
-        type="text"
-        onChange={(e) => handleChange(e)}
-        name="date"
-        value={val.date}
-      />
-      <label>reminder</label>
-      <input
-        className=""
-        id=""
-        type="text"
-        onChange={(e) => handleChange(e)}
-        name="reminder"
-        value={val.reminder}
-      />
+      <span>
+        <label>Time</label>
+        <input
+          className=""
+          id=""
+          type="text"
+          onChange={(e) => handleChange(e)}
+          name="time"
+          value={val.time}
+        />
+      </span>
+      <span>
+        <label>Date</label>
+        <input
+          className=""
+          id=""
+          type="text"
+          onChange={(e) => handleChange(e)}
+          name="date"
+          value={val.date}
+        />
+      </span>
+      <span>
+        <label>Reminder</label>
+        <input
+          className=""
+          id=""
+          type="text"
+          onChange={(e) => handleChange(e)}
+          name="reminder"
+          value={val.reminder}
+        />
+      </span>
       <button className="submitButton" type="submit">
         SUBMIT
       </button>
