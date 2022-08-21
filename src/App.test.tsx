3eee5f83446fcbe3954f7e87b00009ec2e8c56ce
umma <756d6d61@gmail.com>
2022-08-22 00:23:55 +0100

    Basic testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders submit text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const submit = screen.getByText(/submit/i);
+  expect(submit).toBeInTheDocument();
+});
+
+test("renders date", () => {
+  render(<App />);
+  const date = screen.getByText(/date/i);
+  expect(date).toBeInTheDocument();
+});
+
+test("renders time", () => {
+  render(<App />);
+  const time = screen.getByText(/time/i);
+  expect(time).toBeInTheDocument();
 });
